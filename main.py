0752d9976d21c41309ecf481e34cdb4de3def4df
Buffalo Store <95204359+JJTChannel@users.noreply.github.com>
2023-08-24 20:46:55 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 from requests import post,delete
 from re import search
+import requests
 from colorama import Fore
 from os import system
+import random
 from time import sleep
 from pystyle import Add, Center, Anime, Colors, Colorate, Write, System
+import threading
 
-System.Title("JJTxStore - Status : loading")
+
+System.Title("Buffalo Store - Status : loading")
 
 l = Fore.LIGHTRED_EX
 r = Fore.RED
@@ -15,31 +19,53 @@ cy = Fore.CYAN
 b = Fore.BLUE
 w = Fore.WHITE
 
+countries = ["TH","US","AU","SG","UK"]
+
+def webhookspamming(webhook,name,message):
+    try:
+        url = f"http://pubproxy.com/api/proxy?limit=1&format=txt&http=true&country={random.choice(countries)}&type=http&post=true"
+        print(f"{b}Getting Proxies From: {r}{url}")
+        proxy = requests.get(url).text
+        session = requests.Session()
+        session.proxies = {
+            "http": proxy,
+            "https": proxy
+        }
+        session.post(webhook,json={"username": name,"content": message})
+        print(f"{l}Status: {g}Succeed")
+    except:
+        try:
+            post(webhook,json={"username": name,"content": message})
+            print(f"{l}Status: {g}Succeed")
+        except:
+            print(f"{l}Status: {r}Error")
+
 banner = F"""
-░░░░░██╗░░░░░██╗████████╗██╗░░██╗░██████╗████████╗░█████╗░██████╗░███████╗
-░░░░░██║░░░░░██║╚══██╔══╝╚██╗██╔╝██╔════╝╚══██╔══╝██╔══██╗██╔══██╗██╔════╝
-░░░░░██║░░░░░██║░░░██║░░░░╚███╔╝░╚█████╗░░░░██║░░░██║░░██║██████╔╝█████╗░░
-██╗░░██║██╗░░██║░░░██║░░░░██╔██╗░░╚═══██╗░░░██║░░░██║░░██║██╔══██╗██╔══╝░░
-╚█████╔╝╚█████╔╝░░░██║░░░██╔╝╚██╗██████╔╝░░░██║░░░╚█████╔╝██║░░██║███████╗
-░╚════╝░░╚════╝░░░░╚═╝░░░╚═╝░░╚═╝╚═════╝░░░░╚═╝░░░░╚════╝░╚═╝░░╚═╝╚══════╝
-                        Press Enter To Start"""
+
+██████╗░██╗░░░██╗███████╗███████╗░█████╗░██╗░░░░░░█████╗░  ░██████╗████████╗░█████╗░██████╗░███████╗
+██╔══██╗██║░░░██║██╔════╝██╔════╝██╔══██╗██║░░░░░██╔══██╗  ██╔════╝╚══██╔══╝██╔══██╗██╔══██╗██╔════╝
+██████╦╝██║░░░██║█████╗░░█████╗░░███████║██║░░░░░██║░░██║  ╚█████╗░░░░██║░░░██║░░██║██████╔╝█████╗░░
+██╔══██╗██║░░░██║██╔══╝░░██╔══╝░░██╔══██║██║░░░░░██║░░██║  ░╚═══██╗░░░██║░░░██║░░██║██╔══██╗██╔══╝░░
+██████╦╝╚██████╔╝██║░░░░░██║░░░░░██║░░██║███████╗╚█████╔╝  ██████╔╝░░░██║░░░╚█████╔╝██║░░██║███████╗
+╚═════╝░░╚═════╝░╚═╝░░░░░╚═╝░░░░░╚═╝░░╚═╝╚══════╝░╚════╝░  ╚═════╝░░░░╚═╝░░░░╚════╝░╚═╝░░╚═╝╚══════╝
+                      \_____ https://discord.gg/znSSzHnNQj  Press Enter To Start"""
 bannersucceed = F"""
                             Spam Succeeded!
 
-░░░░░██╗░░░░░██╗████████╗██╗░░██╗░██████╗████████╗░█████╗░██████╗░███████╗
-░░░░░██║░░░░░██║╚══██╔══╝╚██╗██╔╝██╔════╝╚══██╔══╝██╔══██╗██╔══██╗██╔════╝
-░░░░░██║░░░░░██║░░░██║░░░░╚███╔╝░╚█████╗░░░░██║░░░██║░░██║██████╔╝█████╗░░
-██╗░░██║██╗░░██║░░░██║░░░░██╔██╗░░╚═══██╗░░░██║░░░██║░░██║██╔══██╗██╔══╝░░
-╚█████╔╝╚█████╔╝░░░██║░░░██╔╝╚██╗██████╔╝░░░██║░░░╚█████╔╝██║░░██║███████╗
-░╚════╝░░╚════╝░░░░╚═╝░░░╚═╝░░╚═╝╚═════╝░░░░╚═╝░░░░╚════╝░╚═╝░░╚═╝╚══════╝
-                        Press Enter To Use Again"""
+██████╗░██╗░░░██╗███████╗███████╗░█████╗░██╗░░░░░░█████╗░  ░██████╗████████╗░█████╗░██████╗░███████╗
+██╔══██╗██║░░░██║██╔════╝██╔════╝██╔══██╗██║░░░░░██╔══██╗  ██╔════╝╚══██╔══╝██╔══██╗██╔══██╗██╔════╝
+██████╦╝██║░░░██║█████╗░░█████╗░░███████║██║░░░░░██║░░██║  ╚█████╗░░░░██║░░░██║░░██║██████╔╝█████╗░░
+██╔══██╗██║░░░██║██╔══╝░░██╔══╝░░██╔══██║██║░░░░░██║░░██║  ░╚═══██╗░░░██║░░░██║░░██║██╔══██╗██╔══╝░░
+██████╦╝╚██████╔╝██║░░░░░██║░░░░░██║░░██║███████╗╚█████╔╝  ██████╔╝░░░██║░░░╚█████╔╝██║░░██║███████╗
+╚═════╝░░╚═════╝░╚═╝░░░░░╚═╝░░░░░╚═╝░░╚═╝╚══════╝░╚════╝░  ╚═════╝░░░░╚═╝░░░░╚════╝░╚═╝░░╚═╝╚══════╝
+                     \_____ https://discord.gg/znSSzHnNQj  Press Enter To Use Again"""
 rainbowbanner = F"""{Fore.LIGHTRED_EX}
-░░░░░██╗░░░░░██╗████████╗██╗░░██╗░██████╗████████╗░█████╗░██████╗░███████╗
-{Fore.RED}░░░░░██║░░░░░██║╚══██╔══╝╚██╗██╔╝██╔════╝╚══██╔══╝██╔══██╗██╔══██╗██╔════╝
-{Fore.YELLOW}░░░░░██║░░░░░██║░░░██║░░░░╚███╔╝░╚█████╗░░░░██║░░░██║░░██║██████╔╝█████╗░░
-{Fore.GREEN}██╗░░██║██╗░░██║░░░██║░░░░██╔██╗░░╚═══██╗░░░██║░░░██║░░██║██╔══██╗██╔══╝░░
-{Fore.CYAN}╚█████╔╝╚█████╔╝░░░██║░░░██╔╝╚██╗██████╔╝░░░██║░░░╚█████╔╝██║░░██║███████╗
-{Fore.BLUE}░╚════╝░░╚════╝░░░░╚═╝░░░╚═╝░░╚═╝╚═════╝░░░░╚═╝░░░░╚════╝░╚═╝░░╚═╝╚══════╝
+██████╗░██╗░░░██╗███████╗███████╗░█████╗░██╗░░░░░░█████╗░  ░██████╗████████╗░█████╗░██████╗░███████╗
+{Fore.RED}██╔══██╗██║░░░██║██╔════╝██╔════╝██╔══██╗██║░░░░░██╔══██╗  ██╔════╝╚══██╔══╝██╔══██╗██╔══██╗██╔════╝
+{Fore.YELLOW}██████╦╝██║░░░██║█████╗░░█████╗░░███████║██║░░░░░██║░░██║  ╚█████╗░░░░██║░░░██║░░██║██████╔╝█████╗░░
+{Fore.GREEN}██╔══██╗██║░░░██║██╔══╝░░██╔══╝░░██╔══██║██║░░░░░██║░░██║  ░╚═══██╗░░░██║░░░██║░░██║██╔══██╗██╔══╝░░
+{Fore.CYAN}██████╦╝╚██████╔╝██║░░░░░██║░░░░░██║░░██║███████╗╚█████╔╝  ██████╔╝░░░██║░░░╚█████╔╝██║░░██║███████╗
+{Fore.BLUE}╚═════╝░░╚═════╝░╚═╝░░░░░╚═╝░░░░░╚═╝░░╚═╝╚══════╝░╚════╝░  ╚═════╝░░░░╚═╝░░░░╚════╝░╚═╝░░╚═╝╚══════╝
 """
 Anime.Fade(Center.Center(banner), Colors.red_to_blue, Colorate.Vertical, enter=True)
 system("clear||cls")
@@ -49,23 +75,27 @@ def spam():
     message = input(F"{r}MESS{w}AGE : ")
     name = input(F"{r}USER{w}NAME : ")
     amount = int(input(f"{y}AMO{w}UNT : "))
+    delay = int(input(f"{b}DE{w}lay : "))
     am = 0
-    print(f"{r}Sta{w}tus : {g}Waiting..")
+    print(f"{r}Sta{w}tus : {g}Waiting For API..")
     sleep(1)
-    System.Title(f"JJTxStore - Status : Spamming To {webhook}")
+    System.Title(f"Buffalo Store - Status : Spamming To {webhook}")
     for i in range(amount):
-        post(webhook,json={"username": name,"content": message})
+        print("\n")
+        sleep(delay)
+        threading.Thread(target=webhookspamming(webhook,name,message)).start()
         am = am + 1
-        print(f"{b}Spamming to {g}-- {w}{webhook} {g}-- {r}Round : ({str(am)}/{str(amount)}){g}")
-        System.Title(f"JJTxStore - Status : Spamming To {webhook} - Round : ({str(am)}/{str(amount)})")
+        print(f"{b}Spamming {r}Round : ({str(am)}/{str(amount)}){g}")
+        print("\n")
+        System.Title(f"Buffalo Store - Status : Spamming To {webhook} - Round : ({str(am)}/{str(amount)})")
     sleep(1)
-    System.Title(f"JJTxStore - Status : Succeed Spam To {webhook} - Amount {amount}")
+    System.Title(f"Buffalo Store - Status : Succeed Spam To {webhook} - Amount {amount}")
     print(f"Succeed Spam To {webhook} - Amount {amount}")
     system("clear||cls")
     Anime.Fade(Center.Center(bannersucceed), Colors.red_to_blue, Colorate.Vertical, enter=True)
-    System.Title(f"JJTxStore - Status : Loading...")
+    System.Title(f"Buffalo Store - Status : Loading...")
     sleep(1)
-    System.Title("JJTxStore - Status : Waiting For Spaming")
+    System.Title("Buffalo Store - Status : Waiting For Spaming")
     system("clear||cls")
     print(Center.Center(rainbowbanner))
     run()
@@ -77,16 +107,19 @@ def deletew():
     print(f"{b}Deleted {webhook}!!")
     sleep(1)
     Anime.Fade(Center.Center(bannersucceed), Colors.red_to_blue, Colorate.Vertical, enter=True)
-    System.Title(f"JJTxStore - Status : Loading...")
+    System.Title(f"Buffalo Store - Status : Loading...")
     sleep(1)
-    System.Title("JJTxStore - Status : Waiting For Spaming")
+    System.Title("Buffalo Store - Status : Waiting For Spaming")
     system("clear||cls")
     print(Center.Center(rainbowbanner))
     run()
 
 def run():
-    print(f"""{cy}1 = Spam Webhook {g}2 = Delete Webhook""")
-    INPUT = input("")
+    print(f"""
+    
+    \_ {cy}1 = Spam Webhook {g}2 = Delete Webhook _/
+    """)
+    INPUT = input(f"  {r}CHO{w}ICE: ")
     if INPUT == "1":
         system("clear||cls")
         spam()
